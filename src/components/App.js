04b24f542bd233fc681ffa519aec98e7e44f49b7
Jimmy Li <jmyli562@gmail.com>
2023-06-10 10:52:07 -0700

    finish lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [image, setImage] = useState("");
+  const [isLoaded, setLoaded] = useState(false);
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setImage(data.message);
+        setLoaded(true);
+      });
+  }, []);
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <>
+      <h3>App</h3>
+      <img src={image} alt="A Random Dog"></img>
+    </>
+  );
+}
+
+export default App;
